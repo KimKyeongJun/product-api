e1fdacd9f9db92a2f2779521e270998bf2fb6b45
김경준 <v49011591@gmail.com>
2024-05-23 22:43:08 +0900

    초기 프로젝트 설정 커밋 - 멀티 모듈 구성 - domain 모듈에 기본 엔티티 추가 및 JPA 설정 진행 중

---
+++ domain/src/main/java/com/example/domain/entity/category/Category.java
@@ -0,0 +1,20 @@
+package com.example.domain.entity.category;
+
+
+import com.example.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
